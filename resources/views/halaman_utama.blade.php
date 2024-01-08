437bc1cbd5ab449ab6a76ba4a4d088a08aa15080
MUHAMMAD BAQIR ATMOJO PRATOMO <muhammad.baqir53@gmail.com>
2024-01-08 11:48:10 +0700

    test

---
+++ resources/views/halaman_utama.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en" data-uw-loader>
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link rel="icon" href="image/logo/logo_app.png">
+        <link rel="apple-touch-icon" href="/logo_app.png">
+        <link rel="manifest" href="/manifest.json">
+        <link rel="stylesheet" href="/css/halaman_utama.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&amp;display=swap" rel="stylesheet">
+        <meta property="og:image" content="/gambar_url.png">
+        <meta name="google-site-verification" content="AaDwuI8WggcxceIy2CIxopuqwFlZMijBCtgylafWNr0">
+        <meta name="robots" content="index, follow">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta name="language" content="English">
+        <meta name="ahrefs-site-verification" content="5d6fadc1fb5853c55b9299ab866ea62ee1a9be89d28eba14e967161304ec39ea">
+        <meta name="title" content="Website Kementerian Pertanahan Nasional">
+        <meta name="viewport" content="width=device-width,initial-scale=1" >
+        <meta name="theme-color" content="#000000">
+        <meta name="description" content="Website Kementerian Agraria / Badan Pertanahan Nasional. PTSL, Daftar PPAT, Website Kementerian Daerah, Layanan Pertanahanan">
+        <meta charset="utf-8">
+        <title>Beranda  | Kantor Pertanahan Kabupaten Banyumas</title>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    </head>
+    <body style="overflow: hidden auto;">
+        <div id="root">
+            <div>
+                <div style="max-height: 50px; background: rgb(0, 62, 105); color: black">
+                    <div class="container social-feeds">
+                        <div class="row" style="color: white">
+                            <div class="col-md-3">
+                                <p class="navigation">
+                                    <i class="fa fa-envelope"></i>&nbsp;
+                                    <span>kab-banyumas@atrbpn.go.id</span>
+                                </p>
+                            </div>
+                            <div class="col-md-3">&nbsp;</div>
+                            <div class="col-md-3">&nbsp;</div>
+                            <div class="col-md-3 text-right"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
